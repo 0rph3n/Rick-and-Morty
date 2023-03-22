dd15a96be4139d1f8a0cfdf48938a426bcb77d30
0rph3n <leandrofarnochi@hotmail.com>
2023-03-22 16:15:04 -0300

    Se realizan cambios en los directorios, se añaden archivos de la HW Webserver

---
+++ end/src/server.js
@@ -0,0 +1,7 @@
+const http = require("http");
+
+http.createServer((req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  const {url} = req;
+  if ()
+}).listen(3001, "localhost");
